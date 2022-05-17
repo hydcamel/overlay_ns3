ee2d65a49044bc70a437c89677d4fe4356e917d8
hydcamel <hyd1123camel@gmail.com>
2022-05-17 09:55:31 -0400

    Add files via upload

--- py_utils.py
+++ py_utils.py
@@ -4,12 +4,15 @@ Created on Sun Apr 24 11:39:41 2022
 
 @author: robbe
 """
+from matplotlib import lines
 import numpy as np
 import random
 import networkx as nx
 import gurobipy as gp
 from gurobipy import GRB
 
+
+'''Create Functions'''
 def create_incidence_from_graph(file_name:str):
     file_to_read = './Data/' + file_name
     graphFile = open(file_to_read, 'r')
@@ -36,7 +39,25 @@ def create_incidence_from_graph(file_name:str):
         # print("idx = ", idx, " src = ", src, " dest = ", dest, " w = ", weight, " bw = ", bw, " d = ", delay)
     return D, edges_bw, edges_delay, edges_list, edges_dict
 
-# def routing_underlay_spr_hop():
+def create_overlay_nodes(n_overlay_nodes, spr_table, n_nodes):
+    spr_mat = create_spr_mat(spr_table=spr_table, n_node=n_nodes)
+    idx_overlay_node = [None] * n_overlay_nodes
+    idx_overlay_node[0] = random.randint(0, n_nodes-1)
+    for i in range(1,n_overlay_nodes):
+        min_id, min_val = 0, 0
+        tmp_it = 0
+        for j in range(n_nodes):
+            tmp_it = np.amin( spr_mat[idx_overlay_node[0:i], j] )
+            if tmp_it > min_val:
+                min_id, min_val = j, tmp_it
+        idx_overlay_node[i] = min_id
+    return idx_overlay_node
+
+def create_tunnel_delay(tunnel_list:list, map_tunnel2edge, edge_delay):
+    tunnel_delays = [None] * len(tunnel_list)
+    for i in range(len(tunnel_list)):
+        tunnel_delays[i] = np.sum( edge_delay[np.nonzero(map_tunnel2edge[:, i])[0].tolist()] )
+    return tunnel_delays
     
 def routing_underlay_spr_delay(edges_list):
     DG_under = nx.DiGraph()
@@ -75,7 +96,7 @@ def create_mapping(tunnel_list, spr_table, edges_dict, n_edges, edges_bw, edges_
         idx_tunnel += 1
     return map_mat, tunnel_capacity, tunnel_delay
 
-def category_perfect_given_tunnels(map_tunnel2edge):
+def create_category_perfect_given_tunnels(map_tunnel2edge):
     category_dict = {} # key = list of tunnels; val = edges in the category
     n_edges = map_tunnel2edge.shape[0]
     for i in range(n_edges):
@@ -89,6 +110,23 @@ def category_perfect_given_tunnels(map_tunnel2edge):
             category_dict[key_str] = [i]
     return category_dict
 
+def create_tunnel_demands(create_type:str, tunnel_capacity:list, n_nodes, idx_overlay_nodes, tunnel_list:list, demand_file=None):
+    n_tunnel = len(tunnel_capacity)
+    lb, ub = 0.5, 0.8
+    tunnel_demands = np.zeros((n_tunnel,))
+    if create_type == 'random':
+        for i in range(n_tunnel):
+            tunnel_demands[i] = round( np.random.uniform(lb*tunnel_capacity[i], ub*tunnel_capacity[i], 1)[0], 1 ) 
+    elif create_type == 'constant':
+        tunnel_demands = ub*tunnel_capacity
+    elif create_type == 'read':
+        tunnel_demands = read_demands_file(demand_file)
+    elif create_type == 'gravity':
+        tunnel_demands = gravity_model_demands(n_nodes=n_nodes, idx_overlay_nodes=idx_overlay_nodes, tunnel_list=tunnel_list)
+    return tunnel_demands.tolist()
+
+
+'''Optimization: Routing'''
 def aware_routing_MinCostFixedRate(routing_type:str, tunnel_capacity:list, tunnel_demands:list, category_dict:dict, nodes_overlay:list, tunnel_list:list, tunnel_delay:list, edge_capacity:list, is_creation_cost = False):
     min_cost_model = gp.Model()
     n_tunnel = len(tunnel_capacity)
@@ -141,100 +179,6 @@ def aware_routing_MinCostFixedRate(routing_type:str, tunnel_capacity:list, tunne
                 res_x[i,j] = xijh[i,j].X
         return res_x
 
-def convert_route_to_file(res_x, tunnel_list, spr_table):
-    # tunnels * demands
-    file_route_name = "route_table.txt"
-    with open(file_route_name, "w") as file_route:
-        for i in range(len(tunnel_list)): # iterate over all SD pairs (demands)
-            src, dest = tunnel_list[i][0], tunnel_list[i][1]
-            list_tunnel_route = np.nonzero(res_x[:,i]) # used tunnels
-            origin = src
-            write_odpair = str(src)+ ' ' + str(dest) + ': '
-            write_route = str(src)
-            list_underlay_route = [src] # a list to store underlay routing path node, which can be used to calculate the size!
-            # file_route.write(str(src)+ ' ' + str(dest) + ': ')
-            # file_route.write(str(src))
-            while origin != dest: # iterate over each tunnel for routing this pair
-                for tunnel in [tunnel_list[k] for k in list_tunnel_route[0].tolist()]: # find the used tunnel whose origin is correct
-                    if (origin == tunnel[0]):
-                        underlay_route = spr_table[origin][tunnel[1]] # underlay path for this specific tunnel within the overlay routing
-                        for j in range(1, len(underlay_route)): # write all underlay path node
-                            write_route += ' ' + str(underlay_route[j])
-                            list_underlay_route.append(underlay_route[j])
-                            # file_route.write(' ' + str(underlay_route[j]))
-                        origin = underlay_route[-1]
-                        break
-            file_route.write(write_odpair + str(len(list_underlay_route)) + ': ' + write_route)
-            file_route.write('\n')
-
-def decode_tunnel_str(tunnel_str):
-    tmp = tunnel_str.strip('[').strip(']').split(', ')
-    tunnel_list = [int(str_to_decode) for str_to_decode in tmp]
-    # tunnel_list = []
-    # for str_to_decode in tmp:
-    #     tunnel_list = [tunnel_list, int(str_to_decode)]
-    return tunnel_list
-
-def create_tunnel_demands(create_type:str, tunnel_capacity:list, n_nodes, idx_overlay_nodes, tunnel_list:list, demand_file=None):
-    n_tunnel = len(tunnel_capacity)
-    lb, ub = 0.5, 0.8
-    tunnel_demands = np.zeros((n_tunnel,))
-    if create_type == 'random':
-        for i in range(n_tunnel):
-            tunnel_demands[i] = round( np.random.uniform(lb*tunnel_capacity[i], ub*tunnel_capacity[i], 1)[0], 1 ) 
-    elif create_type == 'constant':
-        tunnel_demands = ub*tunnel_capacity
-    elif create_type == 'read':
-        tunnel_demands = read_demands_file(demand_file)
-    elif create_type == 'gravity':
-        tunnel_demands = gravity_model_demands(n_nodes=n_nodes, idx_overlay_nodes=idx_overlay_nodes, tunnel_list=tunnel_list)
-    return tunnel_demands.tolist()
-        
-def write_tunnel_demands(tunnel_demands:list, tunnel_list:list):
-    # Write into files
-    filename = "tunnel_demands.txt"
-    with open(filename, "w") as fw:
-        for i in range(len(tunnel_list)):
-            fw.write( str(tunnel_list[i][0]) + ' ' + str(tunnel_list[i][1]) + ' ' + str(tunnel_demands[i]) + '\n' )
-
-def scale_tunnel_demands(init_demands:list, map_tunnel2edge, edge_capacity:list, tunnel_list:list, nodes_overlay:list, tunnel_delays:list, alpha):
-    gp_model = gp.Model()
-    n_tunnel = len(init_demands)
-    xijh = gp_model.addVars(n_tunnel, n_tunnel, vtype = GRB.BINARY, name = 'x_bin') # tunnels * demands
-    omega = gp_model.addVars(1, lb = 0, name = 'omega')
-    '''Capacity Constraint'''
-    for i in range(len(edge_capacity)):
-        tunnels_per_edge = np.nonzero( map_tunnel2edge[i,:] )[0].tolist()
-        tmp = [init_demands[j]*xijh[k, j] for k in range(len(tunnel_list)) if k in tunnels_per_edge for j in range(len(init_demands))]
-        gp_model.addLConstr( gp.quicksum(tmp) <= omega[0] * edge_capacity[i])
-    '''Flow Conservation Constraint'''        
-    for i in range(n_tunnel): #iterate over demands
-        (src, dest) = tunnel_list[i]
-        for node in nodes_overlay: # iterate over overlay nodes
-            tunnel_out = [tunnel_list.index((node, j)) for j in nodes_overlay if node != j]
-            tunnel_in = [tunnel_list.index((j, node)) for j in nodes_overlay if node != j]
-            lhs_list = [xijh[j,i] for j in tunnel_out]
-            rhs_list = [xijh[j,i] for j in tunnel_in]
-            gp_model.addLConstr( gp.quicksum(lhs_list) == gp.quicksum(rhs_list) + 1*(src==node) - 1*(dest==node) )
-        '''For each demand, we constrain the quality'''
-        gp_model.addLConstr( gp.quicksum([tunnel_delays[k] * xijh[k,i] for k in range(n_tunnel)]) <= alpha * tunnel_delays[i] )
-    gp_model.setObjective( omega[0], GRB.MINIMIZE )
-    gp_model.optimize()
-    return [iter_demand / omega[0].X for iter_demand in init_demands]
-        
-    
-
-def read_demands_file(demand_file:str):
-    file_to_read = './Data/' + demand_file
-    demandFile = open(file_to_read, 'r')
-    Lines = demandFile.readlines()
-    
-def wr_overlay_nodes(loc_overlay_nodes:list):
-    file_name = "overlay.txt"
-    str_wr = str(len(loc_overlay_nodes)) + ': ' + ' '.join(str(idx) for idx in loc_overlay_nodes)
-    with open(file_name, "w") as f:
-        f.write( str_wr );
-
 def MinCostFixedRate_v2(routing_type:str, tunnel_capacity:list, tunnel_demands:list, category_dict:dict, nodes_overlay:list, tunnel_list:list, tunnel_delay:list, edge_capacity:list):
     min_cost_model = gp.Model()
     M_oe = 1e6
@@ -272,25 +216,33 @@ def MinCostFixedRate_v2(routing_type:str, tunnel_capacity:list, tunnel_demands:l
             res_x[i,j] = xijh[i,j].X
     return res_x
 
-def create_overlay_nodes(n_overlay_nodes, spr_table, n_nodes):
-    spr_mat = create_spr_mat(spr_table=spr_table, n_node=n_nodes)
-    idx_overlay_node = [None] * n_overlay_nodes
-    idx_overlay_node[0] = random.randint(0, n_nodes-1)
-    for i in range(1,n_overlay_nodes):
-        min_id, min_val = 0, 0
-        tmp_it = 0
-        for j in range(n_nodes):
-            tmp_it = np.amin( spr_mat[idx_overlay_node[0:i], j] )
-            if tmp_it > min_val:
-                min_id, min_val = j, tmp_it
-        idx_overlay_node[i] = min_id
-    return idx_overlay_node
 
-def create_tunnel_delay(tunnel_list:list, map_tunnel2edge, edge_delay):
-    tunnel_delays = [None] * len(tunnel_list)
-    for i in range(len(tunnel_list)):
-        tunnel_delays[i] = np.sum( edge_delay[np.nonzero(map_tunnel2edge[:, i])[0].tolist()] )
-    return tunnel_delays
+
+'''Demands Model'''
+def scale_tunnel_demands(init_demands:list, map_tunnel2edge, edge_capacity:list, tunnel_list:list, nodes_overlay:list, tunnel_delays:list, alpha):
+    gp_model = gp.Model()
+    n_tunnel = len(init_demands)
+    xijh = gp_model.addVars(n_tunnel, n_tunnel, vtype = GRB.BINARY, name = 'x_bin') # tunnels * demands
+    omega = gp_model.addVars(1, lb = 0, name = 'omega')
+    '''Capacity Constraint'''
+    for i in range(len(edge_capacity)):
+        tunnels_per_edge = np.nonzero( map_tunnel2edge[i,:] )[0].tolist()
+        tmp = [init_demands[j]*xijh[k, j] for k in range(len(tunnel_list)) if k in tunnels_per_edge for j in range(len(init_demands))]
+        gp_model.addLConstr( gp.quicksum(tmp) <= omega[0] * edge_capacity[i])
+    '''Flow Conservation Constraint'''        
+    for i in range(n_tunnel): #iterate over demands
+        (src, dest) = tunnel_list[i]
+        for node in nodes_overlay: # iterate over overlay nodes
+            tunnel_out = [tunnel_list.index((node, j)) for j in nodes_overlay if node != j]
+            tunnel_in = [tunnel_list.index((j, node)) for j in nodes_overlay if node != j]
+            lhs_list = [xijh[j,i] for j in tunnel_out]
+            rhs_list = [xijh[j,i] for j in tunnel_in]
+            gp_model.addLConstr( gp.quicksum(lhs_list) == gp.quicksum(rhs_list) + 1*(src==node) - 1*(dest==node) )
+        '''For each demand, we constrain the quality'''
+        gp_model.addLConstr( gp.quicksum([tunnel_delays[k] * xijh[k,i] for k in range(n_tunnel)]) <= alpha * tunnel_delays[i] )
+    gp_model.setObjective( omega[0], GRB.MINIMIZE )
+    gp_model.optimize()
+    return [iter_demand / omega[0].X for iter_demand in init_demands]
 
 def gravity_model_demands(n_nodes, idx_overlay_nodes:list, tunnel_list:list):
     in_traffic, out_traffic = np.zeros((n_nodes,)), np.zeros((n_nodes,))
@@ -305,4 +257,75 @@ def gravity_model_demands(n_nodes, idx_overlay_nodes:list, tunnel_list:list):
         overlay_demands[i] = in_traffic[src] * out_traffic[dest] / sum_product * total_Mbps
     return overlay_demands
 
+'''Write Functions'''
+def convert_route_to_file(res_x, tunnel_list, spr_table):
+    # tunnels * demands
+    file_route_name = "route_table.txt"
+    with open(file_route_name, "w") as file_route:
+        for i in range(len(tunnel_list)): # iterate over all SD pairs (demands)
+            src, dest = tunnel_list[i][0], tunnel_list[i][1]
+            list_tunnel_route = np.nonzero(res_x[:,i]) # used tunnels
+            origin = src
+            write_odpair = str(src)+ ' ' + str(dest) + ': '
+            write_route = str(src)
+            list_underlay_route = [src] # a list to store underlay routing path node, which can be used to calculate the size!
+            # file_route.write(str(src)+ ' ' + str(dest) + ': ')
+            # file_route.write(str(src))
+            while origin != dest: # iterate over each tunnel for routing this pair
+                for tunnel in [tunnel_list[k] for k in list_tunnel_route[0].tolist()]: # find the used tunnel whose origin is correct
+                    if (origin == tunnel[0]):
+                        underlay_route = spr_table[origin][tunnel[1]] # underlay path for this specific tunnel within the overlay routing
+                        for j in range(1, len(underlay_route)): # write all underlay path node
+                            write_route += ' ' + str(underlay_route[j])
+                            list_underlay_route.append(underlay_route[j])
+                            # file_route.write(' ' + str(underlay_route[j]))
+                        origin = underlay_route[-1]
+                        break
+            file_route.write(write_odpair + str(len(list_underlay_route)) + ': ' + write_route)
+            file_route.write('\n')
+
+def write_tunnel_demands(tunnel_demands:list, tunnel_list:list):
+    # Write into files
+    filename = "tunnel_demands.txt"
+    with open(filename, "w") as fw:
+        for i in range(len(tunnel_list)):
+            fw.write( str(tunnel_list[i][0]) + ' ' + str(tunnel_list[i][1]) + ' ' + str(tunnel_demands[i]) + '\n' )
+
+def wr_overlay_nodes(loc_overlay_nodes:list):
+    file_name = "overlay.txt"
+    str_wr = str(len(loc_overlay_nodes)) + ': ' + ' '.join(str(idx) for idx in loc_overlay_nodes)
+    with open(file_name, "w") as f:
+        f.write( str_wr )
         
+def write_setup(graph_file_name):
+    with open("setup.txt", "w") as stf:
+        stf.write(graph_file_name)
+        
+
+'''Read Functions'''
+def read_demands_file(demand_file:str):
+    file_to_read = './Data/' + demand_file
+    demandFile = open(file_to_read, 'r')
+    Lines = demandFile.readlines()
+    
+def read_results(file_name, tunnel_list:list):
+    res_file = open(file_name, 'r')
+    Lines = res_file.readlines()
+    res = [None] * len(tunnel_list)
+    for line in Lines:
+        tmp = line.split(' ')
+        src, dest, val = int(tmp[0]), int(tmp[1]), float(tmp[2])
+        idx = tunnel_list.index((src, dest))
+        res[idx] = val
+    return res
+
+    
+
+'''Miscellaneous'''
+def decode_tunnel_str(tunnel_str):
+    tmp = tunnel_str.strip('[').strip(']').split(', ')
+    tunnel_list = [int(str_to_decode) for str_to_decode in tmp]
+    # tunnel_list = []
+    # for str_to_decode in tmp:
+    #     tunnel_list = [tunnel_list, int(str_to_decode)]
+    return tunnel_list
--- test_category_v1.py
+++ test_category_v1.py
@@ -41,6 +41,7 @@ idx_topology_list = [0]
 
 for idx_topo in idx_topology_list:
     D, edges_bw, edges_delay, edges_list, edges_dict = py_utils.create_incidence_from_graph( all_topology_filename[idx_topo] )
+    py_utils.write_setup(all_topology_filename[idx_topo])
 
     # DG_under = nx.DiGraph()
     # DG_under.add_weighted_edges_from(edges_list)
@@ -60,31 +61,59 @@ for idx_topo in idx_topology_list:
 
     tunnel_list = py_utils.create_overlay_node_tunnel( idx_overlay_node )
     map_tunnel2edge, tunnel_capacity, tunnel_delay = py_utils.create_mapping(tunnel_list=tunnel_list, spr_table=spr_table, edges_dict = edges_dict, n_edges = D.shape[1], edges_bw = edges_bw, edges_delay=edges_delay )
-    category_dict = py_utils.category_perfect_given_tunnels( map_tunnel2edge )
+    category_dict = py_utils.create_category_perfect_given_tunnels( map_tunnel2edge )
     tunnel_delays = py_utils.create_tunnel_delay(tunnel_list=tunnel_list, map_tunnel2edge=map_tunnel2edge, edge_delay=edges_delay)
 
-    delay_direct = np.zeros((3,n_iter_demands))
+    # delay_direct = np.zeros((3,n_iter_demands))
+    # congestion_direct = np.zeros((3,n_iter_demands))
+    # delay_aware = np.zeros((3,n_iter_demands))
+    # congestion_aware = np.zeros((3,n_iter_demands))
+    # delay_agonostic = np.zeros((3,n_iter_demands))
+    # congestion_agonostic = np.zeros((3,n_iter_demands))
+    delay_log = np.zeros((2,n_iter_demands))
+    congestion_log = np.zeros((2,n_iter_demands))
     for it_demand in range(n_iter_demands):
         tunnel_demands = py_utils.create_tunnel_demands(create_type='gravity', tunnel_capacity=tunnel_capacity, n_nodes=D.shape[0], idx_overlay_nodes=idx_overlay_node, tunnel_list=tunnel_list, demand_file=None)
         tunnel_demands = py_utils.scale_tunnel_demands(init_demands=tunnel_demands, map_tunnel2edge=map_tunnel2edge, edge_capacity=edges_bw, tunnel_list=tunnel_list, nodes_overlay=idx_overlay_node, tunnel_delays=tunnel_delays, alpha=alpha)
 
-        # with open("debug_demands.pkl", "rb") as f:
-        #     tunnel_demands = pickle.load(f)
-        
         py_utils.convert_route_to_file(np.identity(len(tunnel_list)), tunnel_list, spr_table)
-        
-            
+        os.system("./waf --run MinCostFixRate")
+        delay_direct = py_utils.read_results(file_name="average_delay.txt", tunnel_list=tunnel_list)
+        congestion_direct = py_utils.read_results(file_name="congestion_cnt.txt", tunnel_list=tunnel_list)
+        print("congestion_direct: ", congestion_direct)
+
+
         res_x_aware = py_utils.aware_routing_MinCostFixedRate(routing_type='aware', tunnel_capacity=tunnel_capacity, tunnel_demands=tunnel_demands, category_dict=category_dict, nodes_overlay=idx_overlay_node, tunnel_list=tunnel_list, tunnel_delay=tunnel_delay, edge_capacity=edges_bw, is_creation_cost = is_creation_cost)
+        py_utils.convert_route_to_file(res_x_aware, tunnel_list, spr_table)
+        os.system("./waf --run MinCostFixRate")
+        delay_aware = py_utils.read_results(file_name="average_delay.txt", tunnel_list=tunnel_list)
+        congestion_aware = py_utils.read_results(file_name="congestion_cnt.txt", tunnel_list=tunnel_list)
+        print("delay_aware: ", [delay_aware[k] / delay_direct[k] for k in range(len(tunnel_list))])
+        print("congestion_aware: ", congestion_aware)
 
         res_x_agonostic = py_utils.aware_routing_MinCostFixedRate(routing_type='agnostic', tunnel_capacity=tunnel_capacity, tunnel_demands=tunnel_demands, category_dict=category_dict, nodes_overlay=idx_overlay_node, tunnel_list=tunnel_list, tunnel_delay=tunnel_delay, edge_capacity=edges_bw)
+        py_utils.convert_route_to_file(res_x_agonostic, tunnel_list, spr_table)
+        os.system("./waf --run MinCostFixRate")
+        delay_agonostic = py_utils.read_results(file_name="average_delay.txt", tunnel_list=tunnel_list)
+        congestion_agonostic = py_utils.read_results(file_name="congestion_cnt.txt", tunnel_list=tunnel_list)
+        print("delay_agonostic: ", [delay_agonostic[k] / delay_direct[k] for k in range(len(tunnel_list))])
+        print("congestion_agonostic: ", congestion_agonostic)
+
+        # delay_per_run = np.zeros((2,len(tunnel_list)))
+        # congestion_per_run = np.zeros((2,len(tunnel_list)))
+        # for k in range(len(tunnel_list)):
+        #     delay_per_run[0,k] = delay_aware[k] / delay_direct[k]
+        #     delay_per_run[1,k] = delay_agonostic[k] / delay_direct[k]
+        #     congestion_per_run[0,k] = congestion_aware[k] / congestion_direct[k]
+        #     congestion_per_run[1,k] = congestion_agonostic[k] / congestion_direct[k]
 
         # res_x = py_utils.MinCostFixedRate_v2(routing_type='aware', tunnel_capacity=tunnel_capacity, tunnel_demands=tunnel_demands, category_dict=category_dict, nodes_overlay=idx_overlay_node, tunnel_list=tunnel_list, tunnel_delay=tunnel_delay, edge_capacity=edges_bw)
 
-        '''Compare theoretical delays'''
-        log_delay_theory = np.zeros( (len(tunnel_list), 3) ) # aware; agnostic; direct tunnel
-        log_delay_theory[:,2] = tunnel_delays
-        for i in range(len(tunnel_list)):
-            log_delay_theory[i, 0] = np.sum(np.array( [res_x_aware[j,i] * tunnel_delays[j] for j in range(len(tunnel_list))] ))
-            log_delay_theory[i, 1] = np.sum(np.array( [res_x_agonostic[j,i] * tunnel_delays[j] for j in range(len(tunnel_list))] ))
+        # '''Compare theoretical delays'''
+        # log_delay_theory = np.zeros( (len(tunnel_list), 3) ) # aware; agnostic; direct tunnel
+        # log_delay_theory[:,2] = tunnel_delays
+        # for i in range(len(tunnel_list)):
+        #     log_delay_theory[i, 0] = np.sum(np.array( [res_x_aware[j,i] * tunnel_delays[j] for j in range(len(tunnel_list))] ))
+        #     log_delay_theory[i, 1] = np.sum(np.array( [res_x_agonostic[j,i] * tunnel_delays[j] for j in range(len(tunnel_list))] ))
 
-        py_utils.convert_route_to_file(res_x_aware, tunnel_list, spr_table)
+        # py_utils.convert_route_to_file(res_x_aware, tunnel_list, spr_table)
